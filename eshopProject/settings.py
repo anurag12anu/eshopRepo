fc3ee4855e0cd289fdfecd78dd442b7982f0275b
anurag12anu <149938549+anurag12anu@users.noreply.github.com>
2024-11-05 09:55:28 +0530

    modify all

--- eshopProject/settings.py
+++ eshopProject/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'eshopProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',  # Use MySQL backend
+        'NAME': 'eshopdata',               # Replace with your actual MySQL database name
+        'USER': 'root',                   # Replace with your MySQL username
+        'PASSWORD': 'Rewa@123',              # Replace with your MySQL password
+        'HOST': 'localhost',  # Your RDS MySQL endpoint
+        'PORT': '3306',          
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
